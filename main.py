31ee32fdec4f6902a3fb84cea70944e4374767a1
HSN <hasan.forghani25@gmail.com>
2023-03-03 18:08:22 +0330

    calculating final dong (icluding rent)

--- main.py
+++ main.py
@@ -18,26 +18,42 @@ df = read_csv('bahman.csv')
 def get_rows_by_name(name):
     return df.loc[df['Name'] == name]
 
+
 def get_total_spent_by_name(name):
     rows = get_rows_by_name(name)
     return rows['Spent'].sum()
 
+print(get_total_spent_by_name('Rey'))
+print(get_total_spent_by_name('Zahra'))
+print(get_total_spent_by_name('HSN'))
+
+
 def get_total_spent():
     return df['Spent'].sum()
+
+print(get_total_spent())
 
 def get_total_for_house():
     rows = df.loc[(df['HSN'] == 1) & (df['Rey'] == 1) & (df['Zahra'] == 1)]
     return rows['Spent'].sum()
 
+print(get_total_for_house())
+
 def get_dong_for_house():
     return get_total_for_house()/3
+
+print(get_dong_for_house())
 
 def get_total_for_us():
     rows = df.loc[(df['HSN'] == 1) & (df['Rey'] == 1) & (df['Zahra'] == 0)]
     return rows['Spent'].sum()
 
+print(get_total_for_us())
+
 def get_dong_for_us():
     return get_total_for_us()/2
+
+print(get_dong_for_us())
 
 def get_dong_for_one_from_who(a,b,names):
     rows = df.loc[(df[b] == 1)]
@@ -51,9 +67,18 @@ def get_dong_for_one_from_who(a,b,names):
     rows =rows.query(qry_str)
 
 
+def my_func(names):
+    zahra = get_dong_for_house() - get_total_spent_by_name('Zahra')
+    hsn = get_dong_for_house() + get_dong_for_us() - get_total_spent_by_name('HSN')
+    rey = get_dong_for_house() + get_dong_for_us() - get_total_spent_by_name('Rey')
+    
+    names[0] = hsn + 2000
+    names[1] = rey + 1550
+    names[2] = zahra + 950
+    
+    return names
 
-
-print()
+print(my_func(names))
 
 
 
